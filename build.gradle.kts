67c3a4c5efa1f97e4a672f8b5e148445750021db
mja00 <teamnirvana2@gmail.com>
2025-06-04 13:16:10 -0400

    chore: bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.6-R0.1-SNAPSHOT")
     implementation("net.kyori:adventure-text-serializer-plain:4.19.0")
     implementation(group = "org.bstats", name = "bstats-bukkit", version = "3.1.0")
 }
@@ -56,7 +56,7 @@ tasks {
 
     runServer {
         dependsOn(shadowJar)
-        minecraftVersion("1.21.5")
+        minecraftVersion("1.21.6")
     }
 
     build {
