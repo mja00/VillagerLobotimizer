b8f9b99ab45c18066bc8517b8e39018ffec15e4b
mja00 <teamnirvana2@gmail.com>
2025-07-17 20:50:13 -0400

    chore: fix id

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ hangarPublish {
         if (tag != null) {
             println("Tag: $tag")
         }
-        id.set("VillagerLobotomy")
+        id.set("VillagerLobotimizer")
         // If the tag is the same as the current project version then we're doing a release publish
         if (tag != null && tag.replace("v", "") == project.version.toString()) {
             println("Tagged and tag matches version, doing a release publish")
